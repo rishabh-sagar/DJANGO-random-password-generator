b781ecfce39f7cac0b84298fef427bc27a5160ca
Naz-pikachu <rishabh.jain.met19@itbhu.ac.in>
2020-10-01 02:07:57 +0530

    fixed bug #1 ensuring if the a particular label is selected then others won't mix up with it in the password generation

--- Password_generator/generator/views.py
+++ Password_generator/generator/views.py
@@ -2,36 +2,42 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import random
 
+
 def password(request):
+    char = list('')
+    chars = list('qwertyuiopasdfghjklzxcvbnm')
+    num = list('0123456798')
+    upperchar = list('QWERTYUIOPASDFGHJKLZXCVBNM')
+    special = list('''!@#%^&*()_+=-{}][}{":.,<>?''')
 
-    chars=list('qwertyuiopasdfghjklzxcvbnm')
-    num=list('0123456798')
-    upperchar=list('QWERTYUIOPASDFGHJKLZXCVBNM')
-    special=list('''!@#%^&*()_+=-{}][}{":.,<>?''')
-
-    length=int(request.GET.get('l', 10))
+    length = int(request.GET.get('l', 10))
 
     if(request.GET.get('uppercase')):
-        chars.extend(upperchar)
+        char.extend(upperchar)
 
     if (request.GET.get('numbers')):
-        chars.extend(num)
+        char.extend(num)
 
     if (request.GET.get('specialcharacter')):
-        chars.extend(special)
+        char.extend(special)
+
+    else:
+        char.extend(chars)
+        char.extend(num)
+        char.extend(special)
 
     password = ''
 
     for x in range(length):
-        password += random.choice(chars)
+        password += random.choice(char)
 
     hi = {
         'password': password
 
     }
-    return render(request,'generator/password.html',hi)
+    return render(request, 'generator/password.html', hi)
+
 
 def index(request):
 
-    return render(request,'generator/index.html')
-# Create your views here.
+    return render(request, 'generator/index.html')
